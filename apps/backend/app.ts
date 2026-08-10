9336acbca1f919ed8a72d16e84430367af7be525
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2026-08-10 15:07:01 +0700

    chore: Setup linter + prettier for frontend and backend flybook

--- apps/backend/app.ts
+++ apps/backend/app.ts
@@ -1,10 +1,10 @@
-import 'dotenv/config';
-import express, { type Express, type Request, type Response } from 'express';
+import "dotenv/config";
+import express, { type Express, type Request, type Response } from "express";
 
 const app: Express = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World')
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
 });
 
 export default app;
---
+++ apps/backend/eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["node_modules", "src/generated/prisma", "dist"],
+  },
+  eslintConfigPrettier,
+);
